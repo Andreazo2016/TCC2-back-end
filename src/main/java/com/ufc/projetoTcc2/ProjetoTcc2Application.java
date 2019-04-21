e887c5102a866b63e3e2b62519ad7d5fc4821672
Andreazo Silva Souza <andreazo2012@gmail.com>
2019-04-21 10:30:51 -0300

    first commit

---
+++ src/main/java/com/ufc/projetoTcc2/ProjetoTcc2Application.java
@@ -0,0 +1,13 @@
+package com.ufc.projetoTcc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTcc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTcc2Application.class, args);
+	}
+
+}
